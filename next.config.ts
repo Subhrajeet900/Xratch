17d0e407d1eb50af82e7ed395fdb9c25f8673fe2
subhrajeet900 <harshpanchal12326@gmail.com>
2026-04-11 21:09:43 +0530

    new

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,10 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-let configToExport = nextConfig;
+const withSerwist = withSerwistInit({
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+  disable: process.env.NODE_ENV === "development",
+});
 
-if (process.env.NODE_ENV !== "development") {
-  const withSerwist = withSerwistInit({
-    swSrc: "src/app/sw.ts",
-    swDest: "public/sw.js",
-  });
-  configToExport = withSerwist(nextConfig);
-}
-
-export default configToExport;
+export default withSerwist(nextConfig);
